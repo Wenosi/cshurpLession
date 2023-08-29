f562845c1daed0e1c505abb4ad64d82cdd0fc42d
Wenosi <hope.pw@bk.ru>
2023-08-30 01:30:59 +0300

    Add new project, hello Maria

---
+++ Example_005_HelloUser_love/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Yeeees> its Maria");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
